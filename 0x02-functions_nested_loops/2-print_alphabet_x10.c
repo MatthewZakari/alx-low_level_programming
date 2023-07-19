faa94b10e34db528499a8ebd843f1b41ae7cf08c
Matthew Zakari <zakarimatthew62@gmail.com>
2023-07-19 13:10:48 -0700

    2-alphabet_x10 correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,18 @@
 void print_alphabet_x10(void)
 {
 
-	char ch;
-	int i;
+char ch;
+int i = 0;
 
-	while (i < 10)
-	{
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-	_putchar(ch);
-	}
-	_putchar('\n');
-	i++;
-	}
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+i++;
+}
+
 
 }
