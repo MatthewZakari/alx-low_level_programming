88da4a4d2bb91723f93103122bd1c0245fb43819
Matthew Zakari <zakarimatthew62@gmail.com>
2023-07-19 12:04:42 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Make alphabet inversely from z to a ten times
+ *
+ * return: always 0
+ */
+void print_alphabet_x10(void)
+{
+
+	char ch;
+	int i;
+
+	while (i < 10)
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	_putchar(ch);
+	}
+	_putchar('\n');
+	i++;
+	}
+
+}
