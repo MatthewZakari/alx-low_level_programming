8d8e647415040855073c04f75a4d804be7bd92c0
Matthew Zakari <zakarimatthew62@gmail.com>
2023-07-19 18:31:02 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add -  adds two integers and returns the result
+ * @n1: The first number to br computed
+ * @n2: The second number to  b e computed
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+{
+
+	return (n1 + n2);
+}
