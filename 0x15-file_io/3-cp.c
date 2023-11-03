fc026e9f60fb33e6c985d32be12f05cf5f283b5c
Matthew Zakari <zakarimatthew62@gmail.com>
2023-11-03 16:35:35 -0700

    C - File I/O correction

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,21 +9,19 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to;
 	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read, bytes_written;
+	ssize_t bytes_read = 0, bytes_written;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
-	if (file_from < 0)
+	if (file_from < 0 || bytes_read < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", argv[1]);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (file_to < 0)
 	{
@@ -31,7 +29,6 @@ int main(int argc, char *argv[])
 		close(file_from);
 		exit(99);
 	}
-
 	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(file_to, buffer, bytes_read);
@@ -43,25 +40,11 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
-	if (bytes_read < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	if (close(file_from) < 0)
+	if (close(file_from) < 0 || close(file_to) < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
-	if (close(file_to) < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
-	}
-
 	return (0);
 }
 
