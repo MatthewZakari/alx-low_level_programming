3f44ff67381e4b7bccf9a7b85bf9f4568957151b
Matthew Zakari <zakarimatthew62@gmail.com>
2024-05-22 08:31:17 -0700

    search algorithms

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -3,37 +3,40 @@
 
 /**
  * jump_list - Searches for a value in a sorted singly
- * 		linked list using the Jump search algorithm
+ *		linked list using the Jump search algorithm
  * @list: Pointer to the head of the list to search in
  * @size: Number of nodes in the list
  * @value: Value to search for
  *
- * Return: Pointer to the first node where value is located, or NULL if not found
+ * Return: Pointer to the first node where value is located,
+ *		or NULL if not found
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t step, prev;
+	size_t step, i;
 	listint_t *node, *prev_node;
 
 	if (!list || size == 0)
 		return (NULL);
 
 	step = sqrt(size);
-	prev = 0;
 	node = list;
 
 	while (node && node->index < size && node->n < value)
 	{
 		prev_node = node;
-		for (prev = node->index; node->next && node->index < prev + step;
-				node = node->next)
-			;
+		for (i = 0; node->next && i < step; i++)
+			node = node->next;
 		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
+
+		if (node->n >= value)
+			break;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n", prev, node->index);
+	printf("Value found between indexes [%lu] and [%lu]\n",
+			prev_node->index, node->index);
 
-	for (node = prev_node; node && node->index <= prev + step && node->n <= value;
+	for (node = prev_node; node && node->index < size && node->n <= value;
 			node = node->next)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
